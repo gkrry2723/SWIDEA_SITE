095b07069887c01dad4b71f93f116d4c10f32ca3
hyunju <gkrry27232@naver.com>
2021-07-21 02:26:35 +0900

    tool 모든 구현 완료

--- sites/urls.py
+++ sites/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path("idea/create", view=views.idea_create, name="idea_create"),
     path("idea/<int:pk>/edit", view=views.idea_edit, name="idea_edit"),
     path("idea/<int:pk>/delete", view=views.idea_delete, name="idea_delete"),
+
+    path("tool", view= views.tool_list, name="tool_list"),
+    path("tool/<int:pk>", view=views.tool_detail, name="tool_detail"),
+    path("tool/create", view=views.tool_create, name="tool_create"),
+    path("tool/<int:pk>/edit", view=views.tool_edit, name="tool_edit"),
+    path("tool/<int:pk>/delete", view=views.tool_delete, name="tool_delete"),
 ]
--- sites/views.py
+++ sites/views.py
@@ -1,4 +1,5 @@
 from sites.forms import IdeaForm
+from sites.forms import ToolForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls.base import reverse
 from .models import Idea
@@ -6,6 +7,7 @@ from .models import Tool
 
 # Create your views here.
 
+# ------------------------------- idea -------------------------------
 def idea_list(request):
     all_ideas = Idea.objects.all()
     ctx = {
@@ -15,8 +17,11 @@ def idea_list(request):
 
 def idea_detail(request, pk):
     idea = get_object_or_404(Idea, id=pk)
+    tool_id = idea.devtool.id
+    print("___________________________________________" ,tool_id)
     ctx = {
-        "idea":idea
+        "idea":idea,
+        "tool_id":tool_id,
     }
     return render(request, template_name="ideas/idea_detail.html", context=ctx)
 
@@ -54,3 +59,56 @@ def idea_delete(request,pk):
     idea = get_object_or_404(Idea, id=pk)
     idea.delete()
     return redirect('sites:idea_list')
+
+
+# ------------------------------- tool -------------------------------
+def tool_list(request):
+    all_tools = Tool.objects.all()
+    ctx = {
+        "all_tools":all_tools
+    }
+    return render(request, template_name="tools/tool_list.html", context=ctx)
+
+def tool_detail(request, pk):
+    tool = get_object_or_404(Tool, id=pk)
+    ideas = Idea.objects.filter(devtool = tool)
+    ctx = {
+        "tool":tool,
+        "ideas":ideas,
+    }
+    return render(request, template_name="tools/tool_detail.html", context=ctx)
+
+def tool_create(request):
+    # 만약 create 페이지에서 create를 누르면
+    if request.method == "POST":
+        form = ToolForm(request.POST, request.FILES)
+        if form.is_valid():
+            idea = form.save()
+            return redirect('sites:tool_list')
+    else:
+        form = ToolForm()
+        ctx = {
+            'form':form
+            }
+        
+        return render(request, template_name="tools/tool_create.html", context = ctx)
+
+def tool_edit(request,pk):
+    tool = get_object_or_404(Tool, id=pk)
+    if request.method == "POST":
+        form = ToolForm(request.POST, instance=tool)
+        if form.is_valid():
+            tool = form.save()
+            return redirect('sites:tool_detail', pk)
+    else:
+        form = ToolForm(instance=tool)
+        ctx = {
+            'form':form
+            }
+        
+        return render(request, template_name="tools/tool_create.html", context = ctx)
+
+def tool_delete(request,pk):
+    tool = get_object_or_404(Tool, id=pk)
+    tool.delete()
+    return redirect('sites:tool_list')
